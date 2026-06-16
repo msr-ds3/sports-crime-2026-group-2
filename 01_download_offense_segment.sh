34d29847d2984fcdc5287d335858895e19ed9263
= <alexiedacosta@yahoo.com>
2026-06-16 14:22:29 -0400

    download offense seg update

--- 01_download_offense_segment.sh
+++ 01_download_offense_segment.sh
@@ -3,3 +3,5 @@
 # Direct URL: https://www.openicpsr.org/openicpsr/project/118281/version/V11/view?path=/openicpsr/118281/fcr:versions/V11/offense_segment_csv_1991_2024.zip&type=file
 
 unzip offense_segment_csv_1991_2024.zip "*2000.csv" "*2001.csv" "*2002.csv" "*2003.csv" "*2004.csv" "*2005.csv"
+
+cat *200[0-5].csv > offense_segment.csv
