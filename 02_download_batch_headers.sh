2988249ea9c80227c4711e4d794cfa8b1abc59c7
Addina Rahaman <raddina24@gmail.com>
2026-06-16 14:21:01 -0400

    updated scripts

--- 02_download_batch_headers.sh
+++ 02_download_batch_headers.sh
@@ -1,1 +1,5 @@
-unzip batch_header_csv_1991_2024.zip "*2000.csv" "*2001.csv" "*2002.csv" "*2003.csv" "*2004.csv" "*2005.csv"
+#!/bin/bash
+
+# Direct URL: https://www.openicpsr.org/openicpsr/project/118281/version/V11/download/terms?path=/openicpsr/118281/fcr:versions/V11/batch_header_csv_1991_2024.zip&type=file
+
+unzip batch_header_csv_1991_2024.zip
